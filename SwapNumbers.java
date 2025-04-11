833d02c894bbb2afc23c988fe96f3528b9a30f94
Jamunajayanna <lle029.jamuna.j@gmail.com>
2025-04-12 04:57:45 +0530

    Jamuna

---
+++ SwapNumbers.java
@@ -0,0 +1,17 @@
+public class SwapNumbers {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        System.out.println("Before swapping:");
+        System.out.println("a = " + a + ", b = " + b);
+
+        // Swapping using a temporary variable
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("After swapping:");
+        System.out.println("a = " + a + ", b = " + b);
+    }
+}
